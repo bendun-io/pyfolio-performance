6d82fbc65707f24634c9e449f62defbdfbddf22e
Fabian Bendun <fabian@bendun.io>
2022-03-02 20:33:05 +0100

    added filter methods

--- pyfolio_performance/classFilters.py
+++ pyfolio_performance/classFilters.py
@@ -27,6 +27,39 @@ class Filters:
         return lambda x: False if x.type in typelist else True
 
     @staticmethod
+    def fYear(year):
+        """
+        :param year: The year to filter for.
+        :type year: int
+
+        :return: A filter function that ensures the entry was made in the specified year.
+        :type: Entry -> bool
+        """
+        return lambda x: True if x.getYear()==year else False
+
+    @staticmethod
+    def fMonth(month):
+        """
+        :param month: The month to filter for.
+        :type month: int
+
+        :return: A filter function that ensures the entry was made in the specified month.
+        :type: Entry -> bool
+        """
+        return lambda x: True if x.getMonth()==month else False
+
+    @staticmethod
+    def fDay(day):
+        """
+        :param day: The day to filter for.
+        :type day: int
+
+        :return: A filter function that ensures the entry was made in the specified day.
+        :type: Entry -> bool
+        """
+        return lambda x: True if x.getDay()==day else False
+
+    @staticmethod
     def fAnd(f1,f2):
         """
         :param f1: First function.
