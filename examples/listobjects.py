337d882b297f4a8954c59744cbf651821dbb1f0c
Fabian Bendun <fabian@bendun.io>
2024-08-09 12:20:26 +0200

    removed the type

--- examples/listobjects.py
+++ examples/listobjects.py
@@ -8,10 +8,14 @@ for account in portPerf.getAccounts():
     print(account.getName() + ": " + str(account.getBalance()))
 print()
 
+num = 0
 print("Transactions of #1 Account:")
 acc = portPerf.getAccounts()[0]
 for transaction in acc.getTransactions():
     print("[" + str(transaction.date) + "] " + transaction.type + ": " + str(transaction.getAmount()))
+    num += 1
+    if num >= 5:
+        break
 print()
 
 print("Depots: #" + str(len(portPerf.getDepots())))
@@ -19,12 +23,24 @@ for depot in portPerf.getDepots():
     print(depot.getName())
 print()
 
+num = 0
 print("Transactions of #1 Depot:")
 acc = portPerf.getDepots()[0]
 for transaction in acc.getTransactions():
     print("[" + str(transaction.date) + "] " + transaction.type + ": " + str(transaction.getAmount()))
+    print("\t" + str(transaction.getSecurity()))
+    num += 1
+    if num >= 5:
+        break
 print()
 
+
+print("New purchases in ")
+# def filter_month(entry, month, year):
+#     if entry.getYear() != year or month != entry.getMonth():
+#         return False
+#     return True
+print()
 
 # print("Securities:")
 # print({x.getName(): x.getLogo()==None for x in portPerf.getSecurities()})
